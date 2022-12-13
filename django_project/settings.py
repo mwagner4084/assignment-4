28e6d9333dd3f62d4d074d151b6d23320ec3515b
Marissa Wagner <mconnell4084@mail.kvcc.edu>
2022-12-13 17:04:30 -0500

    update settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
+    'crispy_forms',
+    'crispy_bootstrap5',
+    # Local
+    'accounts.apps.AccountsConfig',
     'twits.apps.TwitsConfig',
 ]
 
@@ -115,6 +120,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" # new
+CRISPY_TEMPLATE_PACK = "bootstrap5" 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
